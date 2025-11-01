c253b4715ae3e2130c04faf8495290c0c1cc9a13
Arman-2107046 <armanr.rafi@gmail.com>
2025-11-01 08:04:43 +0600

    new app installed

--- superDjango/superDjango/views.py
+++ superDjango/superDjango/views.py
@@ -1,13 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def home(request):
     # return HttpResponse("Hello, World. You are at Django Home")
-    return render(request,'website/index.html')
+    return render(request, "index.html")
 
 
 def about(request):
-    return HttpResponse("Hello, World. You are at Django About")
+    # return HttpResponse("Hello, World. You are at Django About")
+    return render(request, "about.html")
+
 
 def contact(request):
-    return HttpResponse("Hello, World. You are at Django Contact")
+    # return HttpResponse("Hello, World. You are at Django Contact")
+    return render(request, "contact.html")
